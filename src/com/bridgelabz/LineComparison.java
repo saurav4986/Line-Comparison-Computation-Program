30f9b2a799445995b51c448bfc294fe6a3491c86
saurav4986 <saurav.raj4986@gmail.com>
2022-10-18 18:42:13 +0530

    Added: Calculation of length of line.

--- src/com/bridgelabz/LineComparison.java
+++ src/com/bridgelabz/LineComparison.java
@@ -1,4 +1,9 @@
+/*Use Case 1 As a fan of geometry, I want to model a line based on a point consisting of (x, y) co-ordinates
+using the Cartesian system,so that I can calculate its length.
+        - A Length as 2 Points (x1, y1) and (x2, y2) - Length of a Line = sqrt( (x2 - x1) ^ 2 + (y2 - y1) ^ 2)*/
 package com.bridgelabz;
+
+import java.util.Scanner;
 
 public class LineComparison {
     public static void main(String[] args) {
@@ -7,5 +12,20 @@ public class LineComparison {
                 Welcome to Line
                 Comparison Computation
                 Program!""");
+
+        Scanner scanner = new Scanner(System.in);
+
+        // input co=ordinate of line 1
+        System.out.println("Enter the value of X1 in line 1 : ");
+        double x1 = scanner.nextDouble();
+        System.out.println("Enter the value of Y1 in line 1 : ");
+        double y1 = scanner.nextDouble();
+        System.out.println("Enter the value of X2 in line 1 : ");
+        double x2 = scanner.nextDouble();
+        System.out.println("Enter the value of Y2 in line 1 : ");
+        double y2 = scanner.nextDouble();
+
+        double length1 = Math.sqrt(Math.pow((x2 - x1),2) + Math.pow((y2 - y1),2));
+        System.out.println("The length of line 1 is : " + length1);
     }
 }
